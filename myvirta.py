2810142265dbca964aeee96cdc7af454dcf88d2b
Anton Solomko <anthony.solomko@gmail.com>
2019-04-30 16:44:31 +0200

    MyVirta.manage_restaurants service and exceptions added

--- myvirta.py
+++ myvirta.py
@@ -1124,29 +1124,32 @@ class MyVirta(Virta):
             'repair': 1000
             }
         
-        for unit_id in self.units(unit_class_kind=['restaurant', 'educational', 'repair']):
+        for unit_id in self.units(unit_class_kind=['restaurant', 'educational', 'repair', 'service_light']):
             print(unit_id)
             unit = self.unit_summary(unit_id)
+            if unit['name'][:1] == '*':
+                continue
             new_price = calculate_new_price(self.service_history(unit_id), unit['customers'],
-                                            min_price[unit['unit_class_kind']])
+                                            min_price.get(unit['unit_class_kind'], 0))
             if new_price:
                 self.set_service_price(unit_id, new_price)
             
-            contracts = self.supply_contracts(unit_id)
-            orders = self.supply_contracts_to_orders(contracts)
-            for product_id, product in self.supply_products(unit_id).items():
-                per_day = product['per_client'] * unit['customers']
-                fund = max(0, product['stock'] - per_day)
-                to_order = max(0, days * per_day - fund)
-                if to_order > 1.05 * per_day:
-                    to_order = round(1.05 * per_day)
-                for contract_id, contract in contracts(product_id=product_id).items():
-                    available = contract['free_for_buy']
-                    if contract['offer_max_qty'] and contract['offer_max_qty'] < available:
-                        available = contract['offer_max_qty']
-                    orders[contract_id]['quantity'] = min(to_order, available)
-                    to_order -= orders[contract_id]['quantity']
-            self.set_supply_contracts(unit_id, orders)
+            if unit['unit_class_kind'] in ['restaurant', 'educational', 'repair']:
+                contracts = self.supply_contracts(unit_id)
+                orders = self.supply_contracts_to_orders(contracts)
+                for product_id, product in self.supply_products(unit_id).items():
+                    per_day = product['per_client'] * unit['customers']
+                    fund = max(0, product['stock'] - per_day)
+                    to_order = max(0, days * per_day - fund)
+                    if to_order > 1.05 * per_day:
+                        to_order = round(1.05 * per_day)
+                    for contract_id, contract in contracts(product_id=product_id).items():
+                        available = contract['free_for_buy']
+                        if contract['offer_max_qty'] and contract['offer_max_qty'] < available:
+                            available = contract['offer_max_qty']
+                        orders[contract_id]['quantity'] = min(to_order, available)
+                        to_order -= orders[contract_id]['quantity']
+                self.set_supply_contracts(unit_id, orders)
     
     
 if __name__ == '__main__':
@@ -1154,7 +1157,7 @@ if __name__ == '__main__':
     #v.manage_restaurants()
     #v.party_sales()
     #v.set_shops_default_prices()
-    v.set_shops_advertisement()
+    #v.set_shops_advertisement()
     #v.propagate_contracts()
     #v.manage_shops()
     #v.set_shops_innovations(refresh=True)
