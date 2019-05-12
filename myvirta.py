eb57001e04cc4641ad2a343b5623ae9cffecef84
Anton Solomko <anthony.solomko@gmail.com>
2019-05-12 12:20:33 +0200

    Added: MyVirta.generate_retail_reports

--- myvirta.py
+++ myvirta.py
@@ -4,6 +4,7 @@
 
 #from virtonomics import *
 import math, random, time
+import matplotlib.pyplot as plt
 from virtonomics import Virta
 
 
@@ -904,7 +905,7 @@ class MyVirta(Virta):
         min_market_share = 0.01  # минимальная доля рынка
         max_market_share = 0.4  # максимальная доля рынка
         max_market_share_stock = 0.5  # максимальный запас относительно рынка
-        max_sales_adjustment = 0.1  # максимальных шаг изменения продаж
+        max_sales_adjustment = 0.05  # максимальных шаг изменения продаж
         max_price_adjustment = 0.01  # максимальных шаг изменения цены
         elasticity = 20  # эластичность спроса
         sales_price_factor = 2  # множитель к распродажной цене для новых товаров
@@ -1181,8 +1182,55 @@ class MyVirta(Virta):
                 self.set_supply_contracts(unit_id, orders)
     
     
+    def generate_retail_reports(self):
+        data = {}
+        for rec in self.db.execute('SELECT * FROM product_date_report').fetchall():
+            if rec['name'] not in data:
+                data[rec['name']] = {'date': [], 'price': [], 'sold': [], 'revenue': []}
+            data[rec['name']]['date'].append(rec['date'])
+            data[rec['name']]['price'].append(rec['price'])
+            data[rec['name']]['sold'].append(rec['sold'])
+            data[rec['name']]['revenue'].append(rec['revenue']/10**9)
+        
+        for product in data:
+            date = data[product]['date']
+            price = data[product]['price']
+            quantity = data[product]['sold']
+            revenue = data[product]['revenue']
+            
+            fig, ax1 = plt.subplots(figsize=(20,10))
+            color = 'tab:orange'
+            ax1.set_title(product)
+            ax1.set_ylabel('Revenue, $1B', color=color)
+            ax1.set_xticklabels(date, rotation=90)
+            ax1.fill_between(date, 0, revenue, color=color, alpha=0.5)
+            ax1.set_ylim(ymin=0)
+            ax1.tick_params(axis='y', labelcolor=color)
+            ax1.set_title(product)
+            
+            ax3 = ax1.twinx()
+            color = 'tab:blue'
+            ax3.set_ylabel('Price, $', color=color)
+            ax3.plot(date, price, color=color, linewidth=3)
+            ax3.set_ylim(ymin=0)
+            ax3.tick_params(axis='y', labelcolor=color)
+            
+            ax2 = ax1.twinx()
+            color = 'tab:red'
+            ax2.set_ylabel('Sold', color=color)
+            ax2.plot(date, quantity, color=color, linewidth=3)
+            target = self.retail_target_volumes.get(product, 0)
+            ax2.plot(date, [target]*len(date), color=color, linestyle=':', linewidth=2)
+            plt.text(0, target*1.01, 'Target sales: {:,}'.format(target).replace(',', ' '), color=color)
+            ax2.set_ylim(ymin=0)
+            ax2.axes.get_yaxis().set_visible(False)
+            
+            plt.savefig(self.path + 'graph/%s.png' % product)
+    
+    
 if __name__ == '__main__':
     v = MyVirta('olga')
+    v.generate_retail_reports()
     #v.manage_restaurants()
     #v.party_sales()
     #v.set_shops_default_prices()
