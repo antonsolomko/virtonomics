9650329377beda0200a477b26c73363870c73656
Anton Solomko <anthony.solomko@gmail.com>
2019-06-16 15:25:56 +0200

    minor print correction

--- myvirta/employee.py
+++ myvirta/employee.py
@@ -1,5 +1,5 @@
 def set_max_employee_level_all(self, kinds=None):
-        print('Setting max employees level')
+        print('Setting max employees level for %s' % kinds)
         if not kinds:
             kinds = ['educational', 'service_light', 'restaurant', 'repair']
         for unit_id in self.units(unit_class_kind=kinds):
